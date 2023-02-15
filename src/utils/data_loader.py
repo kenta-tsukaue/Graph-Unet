1949d117f2306ee28e163b209b0db8ec0364523e
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2023-02-15 18:10:47 +0900

    print追加

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -36,7 +36,10 @@ class FileLoader(object):
 
     def gen_graph(self, f, i, label_dict, feat_dict, deg_as_tag):
         row = next(f).strip().split()
+        print("row:",row)
         n, label = [int(w) for w in row]
+        print("n:",n)
+        print("label:",label)
         if label not in label_dict:
             label_dict[label] = len(label_dict)
         g = nx.Graph()
