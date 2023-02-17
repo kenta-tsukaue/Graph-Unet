bcd8b4d3b1c55981c943b5028a7b9d0718ebd7bb
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2023-02-17 13:11:36 +0900

    listをnumpyにする際のエラーを解消

--- src/network.py
+++ src/network.py
@@ -20,9 +20,9 @@ class GNet(nn.Module):
         Initializer.weights_init(self)
 
     def forward(self, gs, hs, labels):
-        print("gs:", np.array(gs).shape)
-        print("hs:", np.array(hs).shape)
-        print("labels", np.array(labels).shape)
+        print("gs:", np.array(gs.cpu()).shape)
+        print("hs:", np.array(hs.cpu()).shape)
+        print("labels", np.array(labels.cpu()).shape)
         hs = self.embed(gs, hs)
         logits = self.classify(hs)
         return self.metric(logits, labels)
