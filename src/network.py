ce5d0ae11bb9f1d3ea96edc7b2ed2e302ad0c0be
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2023-03-01 14:21:11 +0900

    numpymwo

--- src/network.py
+++ src/network.py
@@ -20,9 +20,9 @@ class GNet(nn.Module):
         Initializer.weights_init(self)
 
     def forward(self, gs, hs, labels):
-        print("gs:", gs.cpu().numpy().shape)
-        print("hs:", hs.cpu().numpy().shape)
-        print("labels", labels.cpu().numpy().shape)
+        print("gs:", gs.numpy().shape)
+        print("hs:", hs.numpy().shape)
+        print("labels", labels.numpy().shape)
         hs = self.embed(gs, hs)
         logits = self.classify(hs)
         return self.metric(logits, labels)
